f092d2b5bf55bca946cd1d20882b540990f135e0
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-01-25 13:23:55 -0500

    add

---
+++ script11.js
@@ -0,0 +1,51 @@
+
+// length
+//             0      1         2          3          4
+let city = ["pune","mumbai","chennai","bangalore","kolkatta"]
+let q1 = city.length
+console.log(q1)
+
+// push
+let q2 = city.push("jaipur")
+console.log(q2)
+console.log(city)
+
+// unshift
+let q3 = city.unshift("hyderabad")
+console.log(q3)
+console.log(city)
+
+// pop
+let Fruits = ["apple","mango","orange","grape"]
+let q4 = Fruits.pop()
+console.log(q4)
+console.log(Fruits)
+
+// shift
+let q5 = Fruits.shift()
+console.log(q5)
+console.log(Fruits)
+
+// includes
+let vegetables = ["cabbage","potato","carrot","brinjal"]
+let q6 = vegetables.includes("carrot")
+console.log(q6)
+
+// indexof
+ let q7 = vegetables.indexOf("potato")
+ console.log(q7)
+
+ // at
+ let q8 = vegetables.at(3)
+ console.log(q8)
+
+ // reverse
+ let q9 = vegetables.reverse()
+ console.log(q9) // reverse order
+ console.log(vegetables) // reverse order
+
+
+
+
+
+
