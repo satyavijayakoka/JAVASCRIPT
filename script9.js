8510dd2d14f3a0fa0686d549c147ebe16576a70d
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2024-01-24 21:20:27 -0500

    added

---
+++ script9.js
@@ -0,0 +1,32 @@
+
+// arrrays
+//              0         1         2      3     4
+let names = ["chinmay","poorva","sarika","ram","sham"]
+
+// program-1
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[4])
+
+// program-2
+let cities = ["pune","mumbai","bangalore","chennai"]
+let q1 = cities.length
+console.log(q1)
+
+for(let i=0; i<cities.length; i++){
+   // console.log(i)
+    console.log(cities[i])
+}
+
+// program-3
+let country =["India","Nepal","Bangladesh","Srilanka"]
+for(let i=0; i<country.length; i++){
+    console.log(country[i])
+}
+
+for(let i=country.length-1; i>=0;i--){
+    console.log(country[i])
+}
+
